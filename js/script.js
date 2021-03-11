cc22e9e0331d0041c511c518999c45d5a86ad774
Yaroslav <yarpav5646@gmail.com>
2021-03-12 00:57:45 +0500

    Add name of node and simple search

--- js/script.js
+++ js/script.js
@@ -1,26 +1,60 @@
 const app = {
-    data () {
+    data() {
         return {
             counter: 'Заметки',
+            plhnameText: 'Название...',
             plhText: 'О чём думаете?...',
+            nameValue: '',
             inpValue: '',
-            nodes: []
+            nodes: [],
+            searchValue: ''
         }
     },
     methods: {
         onInput(event) {
             this.inpValue = event.target.value;
         },
-        onClick (event) {
-            event.preventDefault();
+        onInputName(event) {
+            this.nameValue = event.target.value;
+        },
+        onClick(event) {
             if (this.inpValue != '') {
-                this.nodes.push(this.inpValue);
+                this.nodes.push({
+                    name: this.nameValue,
+                    node: this.inpValue
+                })
                 this.inpValue = '';
-                console.log(this.nodes);
+                this.nameValue = '';
             }
         },
-        Delete (index) {
+        Delete(index) {
             this.nodes.splice(index, 1);
+        },
+        Search(event) {
+            this.searchValue = event.target.value;
+        }
+    },
+
+    computed: {
+        evenItems() {
+            if (this.searchValue == '') {
+                return this.nodes.filter(i => i);
+            }
+            return this.nodes.filter(i => {
+                for (const ind in i) {
+                    if (i[ind] == this.searchValue) {
+                        return i;
+                    }
+                    if (i[ind] + i[ind + 1] == this.searchValue) {
+                        return i;
+                    }
+                    if (i == this.searchValue) {
+                        return i;
+                    }
+                }
+
+            });
+
         }
     }
 }
